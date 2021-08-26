a4e68bff9b18a9b522e9e43f81be25d2e251358f
amroalbarham <amroalbarham@gmil.com>
2021-08-26 17:10:07 +0300

    first in expo

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default function App() {
+  const [show, setShow] = useState(false);
+  const [colorr, setColor] = useState('red');
+  const change = () => {
+    setShow(!show)
+    setColor('orange')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {show && <Text style={styles.text}>Amro is here ....!</Text>}
+      <Button 
+        onPress={() =>  change() }
+        title="Learn More"
+        color={colorr}
+        accessibilityLabel="Learn more about this purple button"
+      />
+
+    
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +32,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  text: {
+    color: 'black',
+    fontSize: 37,
+    padding: 50,
+    flex: 1
+  },
+  image: {
+    flex: 3,
+  },
+  // button1: {
+  //   position: 'absolute',
+  //   top: 20,
+  //   color: 'blue'
+  // }
 });
+
