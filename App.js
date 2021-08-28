dbbd8f3be8f7a92b6f60c5a08f18cf6b16d5d2f1
amroalbarham <amroalbarham@gmil.com>
2021-08-28 22:44:01 +0300

    finish firs step

--- App.js
+++ App.js
@@ -1,26 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import CarsList from './components/CarsList';
+import Header from './components/Header';
 
 export default function App() {
-  const [show, setShow] = useState(false);
-  const [colorr, setColor] = useState('red');
-  const change = () => {
-    setShow(!show)
-    setColor('orange')
-  }
-
   return (
     <View style={styles.container}>
-      {show && <Text style={styles.text}>Amro is here ....!</Text>}
-      <Button 
-        onPress={() =>  change() }
-        title="Learn More"
-        color={colorr}
-        accessibilityLabel="Learn more about this purple button"
-      />
-
-    
+      <Header />
+      <CarsList />
       <StatusBar style="auto" />
     </View>
   );
@@ -32,21 +20,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-
   },
-  text: {
-    color: 'black',
-    fontSize: 37,
-    padding: 50,
-    flex: 1
-  },
-  image: {
-    flex: 3,
-  },
-  // button1: {
-  //   position: 'absolute',
-  //   top: 20,
-  //   color: 'blue'
-  // }
 });
-
